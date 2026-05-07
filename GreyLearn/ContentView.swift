2257ac2969118b9486f7cfd2a7425e394ecf8068
Emmanuel Omokagbo <emmanuel.omokagbo@gmail.com>
2026-05-07 20:52:54 +0100

    Initial Commit

---
+++ GreyLearn/ContentView.swift
@@ -0,0 +1,25 @@
+//
+// ContentView.swift
+// GreyLearn
+
+//  Created by Emmanuel Omokagbo on 5/7/26
+//  Copyright © 2026 Emmanuel Omokagbo. All rights reserved.
+	
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GreyLearnUITests/GreyLearnUITestsLaunchTests.swift
@@ -0,0 +1,34 @@
+//
+// GreyLearnUITestsLaunchTests.swift
+// GreyLearn
+
+//  Created by Emmanuel Omokagbo on 5/7/26
+//  Copyright © 2026 Emmanuel Omokagbo. All rights reserved.
+	
+
+import XCTest
+
+final class GreyLearnUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
