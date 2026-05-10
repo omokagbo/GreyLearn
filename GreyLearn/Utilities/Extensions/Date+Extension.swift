d0f412ec49d9ce2d9e0e5f77f857091a54045006
Emmanuel Omokagbo <emmanuel.omokagbo@gmail.com>
2026-05-10 14:59:59 +0100

    chore: update greeting

--- GreyLearn/Utilities/Extensions/Date+Extension.swift
+++ GreyLearn/Utilities/Extensions/Date+Extension.swift
@@ -16,16 +16,10 @@ extension Date {
         let hour = Calendar.current.component(.hour, from: self)
         
         switch hour {
-        case 3..<5:
-            return "Go to bed"
-        case 5..<12:
-            return "Good morning"
-        case 12..<17:
-            return "Good afternoon"
-        case 17..<22:
-            return "Good evening"
-        default:
-            return "Hello"
+        case 5..<12:  return "Good morning"
+        case 12..<17: return "Good afternoon"
+        case 17..<23: return "Good evening"
+        default:      return "Good night"
         }
     }
     
